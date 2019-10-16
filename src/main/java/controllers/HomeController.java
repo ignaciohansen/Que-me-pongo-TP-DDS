d1b6fe1cb9defec51a04d896495528db4686cbc0
Fede <federicodamianvelasco@gmail.com>
2019-10-16 16:41:03 -0300

    Se agrega Server
    Se agrega Server, Controller, y utilidades de spark

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+//importar modelos cuando se refactorizen las clases a carpeta modelo
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeController {
+
+	public static ModelAndView usuario(Request req, Response res) {
+		Map<String, Object> view = new HashMap<>();
+
+		return new ModelAndView(view, ".hbs"); //completar viewname con template
+	}
+
+	//agregar modelandview que se necesiten acceder aca
+
+}
---
+++ src/main/java/server/Router.java
@@ -0,0 +1,30 @@
+package server;
+
+import controllers.HomeController;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.utils.BooleanHelper;
+import spark.utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+    private static HandlebarsTemplateEngine engine;
+
+    private static void initEngine() {
+        Router.engine = HandlebarsTemplateEngineBuilder
+                .create()
+                .withDefaultHelpers()
+                .withHelper("isTrue", BooleanHelper.isTrue)
+                .build();
+    }
+
+    static void init() {
+        Router.initEngine();
+        Spark.staticFileLocation("/public");
+        Router.configure();
+    }
+
+    private static void configure(){
+        Spark.get("/",HomeController::,engine); //completar homecontroller
+
+    }
+}
