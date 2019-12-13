ea4be8ce5aadce0b4acd511b36dbc848d5a9534d
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2019-12-13 00:01:05 -0300

    Login nuevo y home

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 	public ModelAndView mostrarHome(Request request, Response response) {
 		LoginController.ensureUserIsLoggedIn(request, response);
 		Map<String, Object> parametros = new HashMap<>();
-		return new ModelAndView(parametros, "home.hbs");
+		return new ModelAndView(parametros, "home_nuevo.hbs");
 	}
 }
